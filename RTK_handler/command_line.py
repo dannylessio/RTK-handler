1174d3aaecaa3d8cc657b2aafcf5192bec961421
dannylessio <danny.lessio@gmail.com>
2016-03-17 01:25:14 +0100

    added argparse feature, added reconstruction calls, improved general redability rewriting some functions

--- RTK_handler/command_line.py
+++ RTK_handler/command_line.py
@@ -1,45 +1,83 @@
 import sys
+import argparse
 from .tools import insert_RTK_path, assert_RTK_path
 from .tools import make_structure, assert_structure, clean_structure
-from .tools import print_help
+from .tools import ask_for_projection_source
+from .tools import fdk_default_reconstruction
 from .geometry_maker import create_geometry
 from .projections_handler import normalize_projections
 
-# Try to get the first parameter, if it does not exist print_help
-try:
-    option = sys.argv[1]
-except:
-    option = sys.argv[0]
-
-
 def main():
-    if option   == "help":
-        print_help()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', action='store_true', default=False,
+                        dest='make_structure',
+                        help='Create the folder structure.')
+
+    parser.add_argument('-g', action='store_true', default=False,
+                        dest='create_geometry',
+                        help='Create the .xml geometry reading from csv file.')
+
+    parser.add_argument('-n', action='store_true', default=False,
+                        dest='normalize_projections',
+                        help='Normalize the mha set of projections reading from a csv file.')
+
+    parser.add_argument('-c', action='store_true', default=False,
+                        dest='clean_structure',
+                        help='Delete the folder structure, preserve relevant data.')
+
+    parser.add_argument('-p', action='store_true', default=False,
+                        dest='insert_rtk_path',
+                        help='Insert the path relative to RTK-bin folder.')
+
+    parser.add_argument('-r', action='store_true', default=False,
+                        dest='rtkfdk_reconstruction',
+                        help='Reconstruct with rtkfdk')
+
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+
+    # Print the help if no options are given
+    if len(sys.argv) < 2:
+        parser.print_help()
         sys.exit(0)
 
-    elif option == "make-struct":
+    # If there are some options, parse them
+    options = parser.parse_args()
+
+    # Chech if the parsed options are True
+    if options.make_structure:
         make_structure()
         sys.exit(0)
 
-    elif option == "create-geom":
+    if options.create_geometry:
         assert_structure()
+        #assert_csv()
         create_geometry()
         sys.exit(0)
 
-    elif option == "norm-proj":
+    if options.normalize_projections:
         assert_structure()
+        #assert_csv()
         normalize_projections()
         sys.exit(0)
 
-    elif option == "insert-RTK-path":
+    if options.insert_rtk_path:
         insert_RTK_path()
         sys.exit(0)
 
-    elif option == "clean-structure":
+    if options.clean_structure:
         assert_structure()
         clean_structure()
         sys.exit(0)
 
-    else:
-        print_help()
-        sys.exit(1)
+    if options.rtkfdk_reconstruction:
+        assert_structure()
+        assert_RTK_path()
+        source = ask_for_projection_source()
+        #assert_projection_source(source)
+        fdk_default_reconstruction( source )
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
--- RTK_handler/tools.py
+++ RTK_handler/tools.py
@@ -1,11 +1,68 @@
 import os
+from os.path import expanduser
 import sys
 import pickle
 import subprocess
 import shutil
 
-def print_help():
-    print('\nUsage: RTK-handler opt\n\nopt-0 : help\nopt-1 : make-struct\nopt-2 : create-geom\nopt-3 : norm-proj\nopt-4 : insert-RTK-path\nopt-5 : clean-structure\n')
+home = expanduser("~")
+__pkl_file_path = os.path.join( home, '.RTK_handler.pkl' )
+
+'''
+    __path_of contains this folder structure:
+
+    .
+    ├── csv
+    ├── geometry
+    ├── projections
+    │   ├── non_normalized
+    │   │   ├── mha
+    │   │   └── set_of_tiff
+    │   └── normalized
+    │       ├── mha
+    │       └── set_of_tiff
+    └── reconstructions
+        └── from
+            ├── mha_normalized_proj
+            └── tif_without_norm_proj
+'''
+
+__path_of = {
+    'csv' : os.path.join('csv') ,
+    'geometry' : os.path.join('geometry') ,
+    'projections' : os.path.join('projections') ,
+    'projections_non_normalized' : os.path.join('projections', 'non_normalized') ,
+    'projections_non_normalized_mha' : os.path.join('projections', 'non_normalized','mha') ,
+    'projections_non_normalized_set_of_tiff' : os.path.join('projections', 'non_normalized','set_of_tiff') ,
+    'projections_normalized' : os.path.join('projections', 'normalized') ,
+    'projections_normalized_mha' : os.path.join('projections', 'normalized','mha') ,
+    'projections_normalized_set_of_tiff' : os.path.join('projections', 'normalized','set_of_tiff') ,
+    'reconstructions' : os.path.join('reconstructions') ,
+    'reconstructions_from' : os.path.join('reconstructions','from') ,
+    'reconstructions_from_mha_normalized_proj' : os.path.join('reconstructions','from','mha_normalized_proj') ,
+    'reconstructions_from_tiff_without_norm_proj' : os.path.join('reconstructions','from','tiff_without_norm_proj')
+    }
+
+def execute_bin_inside_RTK_bin( command ):
+    # pick up the RTK-bin path from pickle
+    f = open(__pkl_file_path, "rb")
+    readed = pickle.load( f )
+    f.close()
+
+    # enter to the bin folder
+    rtk_path = readed['rtk_path']
+    rtk_bin_path = os.path.join(rtk_path, 'bin')
+
+    # assembly the command
+    command_name = command[0]
+    full_command = os.path.join(rtk_bin_path, command_name)
+    command[0] = full_command
+
+    # execute, wait and print to console
+    popen = subprocess.Popen(command, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    print( str(output) )
 
 
 ''' Reads from keyboard the RTK-bin absolute path and stores it inside conf.py '''
@@ -23,7 +80,7 @@ def insert_RTK_path():
         stored = { 'rtk_path' : abs_path }
 
         # Write this path on file in binary mode, Overwrites the file if it exists.
-        f = open(".store.pkl", "wb")
+        f = open(__pkl_file_path, "wb")
         pickle.dump(stored, f)
         f.close()
 
@@ -36,7 +93,7 @@ def insert_RTK_path():
 
 def assert_RTK_path():
     try:
-        f = open(".store.pkl", "rb")
+        f = open(__pkl_file_path, "rb")
         f.close()
     except:
         print("Please run the setup option first.\n")
@@ -44,86 +101,48 @@ def assert_RTK_path():
 
 
 '''
-It creates the following structure:
-.
-├── csv
-├── geometry
-├── projections
-│   ├── non_normalized
-│   │   ├── mha
-│   │   └── set_of_tiff
-│   └── normalized
-│       ├── mha
-│       └── set_of_tiff
-└── reconstructions
-    └── from
-        ├── mha_normalized_proj
-        └── tif_without_norm_proj
+    Creates the folder structure:
 '''
 def make_structure():
     try:
-        os.mkdir(os.path.join('csv'))
-        os.mkdir(os.path.join('geometry'))
-        os.mkdir(os.path.join('projections'))
-        os.mkdir(os.path.join('projections', 'non_normalized'))
-        os.mkdir(os.path.join('projections', 'non_normalized','mha'))
-        os.mkdir(os.path.join('projections', 'non_normalized','set_of_tiff'))
-        os.mkdir(os.path.join('projections', 'normalized'))
-        os.mkdir(os.path.join('projections', 'normalized','mha'))
-        os.mkdir(os.path.join('projections', 'normalized','set_of_tiff'))
-        os.mkdir(os.path.join('reconstructions'))
-        os.mkdir(os.path.join('reconstructions','from'))
-        os.mkdir(os.path.join('reconstructions','from','mha_normalized_proj'))
-        os.mkdir(os.path.join('reconstructions','from','tiff_without_norm_proj'))
+        for directory in sorted( __path_of ):
+            os.mkdir( __path_of[ directory ] )
+
         print("Structure created successfully.")
 
     except:
         print("Error on creating structure.")
         sys.exit(0)
 
-
+'''
+    Check if the folder structure exist
+'''
 def assert_structure():
     try:
-        if not os.path.exists(os.path.join('csv')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('geometry')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('projections')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('projections', 'non_normalized')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('projections', 'non_normalized','mha')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('projections', 'non_normalized','set_of_tiff')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('projections', 'normalized')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('projections', 'normalized','mha')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('projections', 'normalized','set_of_tiff')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('reconstructions')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('reconstructions','from')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('reconstructions','from','mha_normalized_proj')) :
-            raise ValueError("Directory does not exist")
-        if not os.path.exists(os.path.join('reconstructions','from','tiff_without_norm_proj')) :
-            raise ValueError("Directory does not exist")
+        for directory in sorted( __path_of ):
+            if not os.path.exists( __path_of[ directory ] ) :
+                raise ValueError( "Directory does not exist" )
 
     except ValueError as e:
         print(e)
         sys.exit(1)
 
+
 '''
-    Clean the previous generated structure
+    Cleans the folder structure
 '''
 def clean_structure():
 
-    # copy relevant files inside folders
+    # copy relevant files inside os.getcwd() folder
     dest = os.getcwd()
 
-    srcList = [os.path.join('csv'), os.path.join('geometry'), os.path.join('projections','non_normalized','mha')]
+    srcList = [
+        __path_of['csv'] ,
+        __path_of['geometry'],
+        __path_of[ 'projections_non_normalized_mha' ],
+        __path_of[ 'reconstructions_from_mha_normalized_proj'] ,
+        __path_of['reconstructions_from_tiff_without_norm_proj']
+        ]
 
     for src in srcList:
         src_files = os.listdir(src)
@@ -133,8 +152,120 @@ def clean_structure():
                 shutil.move(full_file_name, dest)
 
     # delete folders
-    shutil.rmtree(os.path.join('csv'))
-    shutil.rmtree(os.path.join('geometry'))
-    shutil.rmtree(os.path.join('projections'))
-    shutil.rmtree(os.path.join('reconstructions'))
+
+    shutil.rmtree( __path_of[ 'csv' ] )
+    shutil.rmtree( __path_of[ 'geometry' ] )
+    shutil.rmtree( __path_of[ 'projections'] )
+    shutil.rmtree( __path_of['reconstructions'] )
     print("Structure successfully cleaned.")
+
+
+def ask_for_projection_source():
+    inputString = "Reconstruct from:\n0 : normalized mha\n1 : non-normalized tiff set\n"
+
+    # Get the option
+    try:
+        source = input(inputString)
+
+        # remember
+        if int(source) not in range(0,2):
+            raise ValueError
+
+        return source
+
+    except:
+        print("Error on input.")
+
+#def assert_projection_source( source ):
+
+
+def fdk_default_reconstruction( source ):
+
+    # from normalized mha
+    if int(source) == 0:
+        rootFolder = os.getcwd()
+
+        projections_folder = os.path.join(rootFolder, __path_of['projections_normalized_mha'] )
+        projections_name = str( os.listdir( projections_folder )[0] )
+
+        output_path = os.path.join( rootFolder, __path_of['reconstructions_from_mha_normalized_proj'], 'normalized_recon.mha' )
+
+        geometry_path = os.path.join( rootFolder, __path_of['geometry'], 'geometry.xml' )
+
+        spacing_x = str( input( "Insert spacing on x direction\n" ) )
+        spacing_y = str( input( "Insert spacing on y direction\n" ) )
+        spacing_z = str( input( "Insert spacing on z direction\n" ) )
+
+        dimension_x = str( input( "insert x dimension\n" ) )
+        dimension_y = str( input( "insert y dimension\n" ) )
+        dimension_z = str( input( "insert z dimension\n" ) )
+
+
+        #command = 'rtkfdk -p ' + projections_folder + ' -r ' + projections_name + ' -o ' + output_path + ' -g ' + geometry_path + ' --spacing ' + spacing_x + ' ' + spacing_y + ' ' + spacing_z + ' --dimension ' + dimension_x + ' ' + dimension_y + ' ' + dimension_z + ' -v'
+        command = [
+            'rtkfdk',
+            '-p',
+            str(projections_folder),
+            '-r',
+            str(projections_name),
+            '-o',
+            str(output_path),
+            '-g',
+            str(geometry_path),
+            '--spacing',
+            str(spacing_x),
+            str(spacing_y),
+            str(spacing_z),
+            '--dimension',
+            str(dimension_x),
+            str(dimension_y),
+            str(dimension_z),
+            '-v'
+        ]
+
+        print("Reconstructing and writing...")
+        execute_bin_inside_RTK_bin( command )
+
+    # from non-normalized tiff set
+    else:
+        rootFolder = os.getcwd()
+
+        projections_folder = os.path.join(rootFolder, __path_of['projections_normalized_set_of_tiff'] )
+
+        output_path = os.path.join( rootFolder, __path_of['reconstructions_from_tiff_without_norm_proj'], 'non_norm_tiff_recon.mha' )
+
+        geometry_path = os.path.join( rootFolder, __path_of['geometry'], 'geometry.xml' )
+
+        spacing_x = str( input( "Insert spacing on x direction\n" ) )
+        spacing_y = str( input( "Insert spacing on y direction\n" ) )
+        spacing_z = str( input( "Insert spacing on z direction\n" ) )
+
+        dimension_x = str( input( "insert x dimension\n" ) )
+        dimension_y = str( input( "insert y dimension\n" ) )
+        dimension_z = str( input( "insert z dimension\n" ) )
+
+
+        #command = 'rtkfdk -p ' + projections_folder + ' -r ' + projections_name + ' -o ' + output_path + ' -g ' + geometry_path + ' --spacing ' + spacing_x + ' ' + spacing_y + ' ' + spacing_z + ' --dimension ' + dimension_x + ' ' + dimension_y + ' ' + dimension_z + ' -v'
+        command = [
+            'rtkfdk',
+            '-p',
+            str(projections_folder),
+            '-r',
+            '.*.tiff',
+            '-o',
+            str(output_path),
+            '-g',
+            str(geometry_path),
+            '--spacing',
+            str(spacing_x),
+            str(spacing_y),
+            str(spacing_z),
+            '--dimension',
+            str(dimension_x),
+            str(dimension_y),
+            str(dimension_z),
+            '-v'
+        ]
+
+        print("Reconstructing and writing...")
+        execute_bin_inside_RTK_bin( command )
